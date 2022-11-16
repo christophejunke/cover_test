4fe0b383145571e607efe5bd54e44a82af5bbdc1
Christophe Junke <junke.christophe@gmail.com>
2022-11-16 11:35:03 +0100

    Test

--- main.c
+++ main.c
@@ -1,4 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include <sys/stat.h>
+
+struct foo {
+    char* uid;
+};
+
+int fill(struct foo *f)
+{
+    int res = mkdir("/bin/file", 0611);
+    if (res < 0)
+        return -errno;
+    f->uid = strdup("ok");
+    if (f->uid == NULL)
+        return -EINVAL;
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
+    struct foo f = {0};
+    int res = fill(&f);
+    if (res < 0)
+        return 255;
+    printf("%s\n", f.uid);
+    free(f.uid);
     return 0;
 }
