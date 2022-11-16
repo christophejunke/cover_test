d07551d95ff3e64fc08321dc6eb4163645c24fae
Christophe Junke <junke.christophe@gmail.com>
2022-11-16 13:54:42 +0100

    Change mkdir spec, tweak eva options

--- Makefile
+++ Makefile
@@ -1,1 +1,10 @@
 all: main
+
+.PHONY: frama-c
+
+frama-c:
+	frama-c main.c \
+		~/.opam/4.14.0/share/frama-c/libc/string.c \
+		-eva-slevel=3 \
+		-eva-partition-history=2 \
+		-eva
--- main.c
+++ main.c
@@ -3,30 +3,43 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#include <sys/stat.h>
+/* copied and modified from 4.14.0 frama-c libc */
+
+/*@
+  requires valid_path: valid_read_string(path);
+  assigns \result \from indirect:path, indirect:path[0..strlen(path)],
+                        indirect:mode;
+  ensures \result == 0 || (\result == -1 && errno \in {EACCES, EEXIST, ELOOP, EMLINK, ENAMETOOLONG, ENOENT, ENOSPC,ENOTDIR, EROFS});
+*/
+extern int mkdir(const char *path, mode_t mode);
 
 struct foo {
-    char* uid;
+    char* ok;
 };
 
-int fill(struct foo *f)
+int init(struct foo *f)
 {
-    int res = mkdir("/bin/file", 0611);
-    if (res < 0)
-        return -errno;
-    f->uid = strdup("ok");
-    if (f->uid == NULL)
-        return -EINVAL;
-    return 0;
+    int res = 0;
+    res = mkdir("/bin/cover_test", 0611);
+    if (res < 0) {
+        res = -errno;
+        goto out;
+    }
+    f->ok = strdup("o_");
+    if (f->ok == NULL)
+        res = -EINVAL;
+out:
+    return res;
 }
 
 int main(int argc, char **argv)
 {
-    struct foo f = {0};
-    int res = fill(&f);
+    struct foo f;
+    int res = init(&f);
     if (res < 0)
         return 255;
-    printf("%s\n", f.uid);
-    free(f.uid);
+    f.ok[1] = 'k';
+    printf("%s\n", f.ok);
+    free(f.ok);
     return 0;
 }
